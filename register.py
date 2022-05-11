2c6f56d77d7e0cf173d800af25946491c4d9b82a
Zack Feldstein <zfeldstein@Zacks-MBP.satx.rr.com>
2022-05-10 22:26:46 -0500

    Working manifest url with sleeps

--- register.py
+++ register.py
@@ -1,6 +1,7 @@
 import httpx
 import os
 import json
+import time
 
 RANCHER_HOST = os.environ["RANCHER_HOST"]
 RANCHER_API_VERSION = "v3"
@@ -15,15 +16,14 @@ headers = {
     "Content-Type": "application/json",
 }
 
+
 def rancher_api_call(
     rancher_url, request_payload=None, http_method="get", headers=headers
 ):
     rancher_url = "{}{}".format(RANCHER_URL, rancher_url)
     # GET requests to Rancher API
     if http_method.lower() == "get":
-        response = httpx.get(
-            rancher_url, headers=headers, verify=SSL_VERIFY
-        )
+        response = httpx.get(rancher_url, headers=headers, verify=SSL_VERIFY)
         return response.content
     # POST requests to Rancher API
     if http_method.lower() == "post":
@@ -33,13 +33,15 @@ def rancher_api_call(
         try:
             response.raise_for_status()
         except httpx.HTTPStatusError as exc:
-            print(f"Error response {exc.response.status_code} while requesting {exc.request.url!r}.")
+            print(
+                f"Error response {exc.response.status_code} while requesting {exc.request.url!r}."
+            )
             return response.status_code
 
         return response.content
-    
+
     # UnComment if needed for other calls
-    
+
     # if http_method.lower() == "delete":
     #     response = httpx.delete(url, auth=auth)
     # if http_method.lower() == "put":
@@ -65,23 +67,33 @@ def register_cluster(cluster_name):
         "annotations": {},
     }
 
-    # response = rancher_api_call(rancher_url, request_payload=request_payload, http_method='post')
-    # response = json.loads(response)
-    response = {"id": "c-cq4c7"}
+    response = rancher_api_call(
+        rancher_url, request_payload=request_payload, http_method="post"
+    )
+    response = json.loads(response)
+    time.sleep(1)
     # # This will get the registration Token ID
-    registration_payload = {"type":"clusterRegistrationToken","clusterId": response['id']}
+    registration_payload = {
+        "type": "clusterRegistrationToken",
+        "clusterId": response["id"],
+    }
     # URL will look something like https://<HOST_GOES_HERE>/v3/clusters/<ID>/clusterregistrationtokens"
     registration_url = "clusterregistrationtokens"
-    registration_resp = rancher_api_call(registration_url, request_payload=registration_payload, http_method='post')
+    registration_resp = rancher_api_call(
+        registration_url, request_payload=registration_payload, http_method="post"
+    )
     print(registration_resp)
     registration_resp = json.loads(registration_resp)
-    registratio_id = registration_resp['id']
-    
+    registratio_id = registration_resp["id"]
+    time.sleep(1)
+
     # This will get the k8s manifests
     registration_manifest_url = "clusterRegistrationTokens/{}".format(registratio_id)
-    registration_manifest_resp = rancher_api_call(registration_manifest_url,  http_method='get')
+    registration_manifest_resp = rancher_api_call(
+        registration_manifest_url, http_method="get"
+    )
     registration_manifest_resp = json.loads(registration_manifest_resp)
-    print(registration_manifest_resp['manifestUrl'])
+    print(registration_manifest_resp["manifestUrl"])
 
 
-register_cluster("test-import")
+register_cluster("test-imp3o3323r4t7")
